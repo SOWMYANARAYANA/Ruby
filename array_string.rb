3a51345c87cb3138d44ae54db2d081f6c00a57e5
kuldeep_surendra <ksurendra@qwinix.io>
2018-03-30 16:31:35 +0530

    add

---
+++ array_string.rb
@@ -0,0 +1,6 @@
+my_details=["Sowmya","GMIT","qwinix"]
+print my_details
+# my_details=%w(Sowmya,GMIT,Qwinix Technology)
+# my_details.join("")
+# ("Sowmya,GMIT,Qwinix Technology").split
+# puts my_details
